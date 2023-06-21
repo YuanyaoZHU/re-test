3f5b64783dd3e030a54b77a927ce741799afa06a
YuanyaoZHU <zhuyuanyao_only@hotmail.com>
2023-06-21 15:24:00 +0800

    first commit

---
+++ Spar_5MW_FOWT_test/StartTim.bat
@@ -0,0 +1,11 @@
+cd E:\SCI simulation\simulation_file_20211208\LC1.2 Eigenanalysis\TimoSimulation\Yaw\Yaw
+E:
+AeroDynZhu.exe NRELOffshrBsline5MW_Onshore_Test01.dvr
+
+cd E:\SCI simulation\simulation_file_20211208\LC1.2 Eigenanalysis\TimoSimulation\Yaw\Yaw
+E:
+FloatingWindTurbine.FloaterZhu.TiMO_V1.exe
+
+cd E:\SCI simulation\simulation_file_20211208\LC1.2 Eigenanalysis\TimoSimulation\Yaw\Yaw
+E:
+StartExe.exe
